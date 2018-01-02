1665d9c3f99afe82b0f0bdc4390da6ce6f4953a2
SmartHaung <464510690@qq.com>
2018-01-02 11:11:32 +0800

    init

---
+++ examapi/src/main/java/com/guanchen/examapi/ExamapiApplication.java
@@ -0,0 +1,12 @@
+package com.guanchen.examapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamapiApplication.class, args);
+	}
+}
---
+++ examconfig/config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  API_ROOT: '"http://huangwenbin.tpddns.cn:8081"'
+}
